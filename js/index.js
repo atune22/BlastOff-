5efbdfcd9d4031a3bab86e93aa75e7a319f1436d
atune22 <atune22@punahou.edu>
2016-07-07 11:48:52 -1000

    added astroid
    added astroid and made it go to random places

--- js/index.js
+++ js/index.js
@@ -1,11 +1,28 @@
+var rockMargin;
 
-	var num = 5;
+var rockArray = [550,700,850,1000,1150]
 
-	var rightMove = function () {
-		num += 100;
-		document.getElementById('ship').style.marginLeft = num + "px";
-	}
-	var leftMove = function () {
-		num -= 100;
-		document.getElementById('ship').style.marginLeft = num + "px";
-	}
+var num = 850;
+
+var rightMove = function () {
+	num += 100;
+	document.getElementById('ship').style.marginLeft = num + "px";
+}
+var leftMove = function () {
+	num -= 100;
+	document.getElementById('ship').style.marginLeft = num + "px";
+}
+
+var rockPosition = function(){
+	rockMargin = Math.floor(Math.random()*5);
+	console.log(rockMargin);
+	document.getElementById('rockThing').style.marginLeft = rockArray[rockMargin] + "px";
+}
+rockPosition()
+
+
+
+}
+function leftArrowPressed() {
+	var element = document.getElementById("image1");
+	element.style.left = parseInt(element.style,left) - 5 + 'px';

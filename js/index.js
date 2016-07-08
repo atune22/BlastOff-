f064d0a6d996f2aa3d25e3f6807ed4e562519e8f
atune22 <atune22@punahou.edu>
2016-07-08 11:55:12 -1000

    Made astroid move
    Still not really working but a good start

--- js/index.js
+++ js/index.js
@@ -2,27 +2,49 @@ var rockMargin;
 
 var rockArray = [550,700,850,1000,1150]
 
-var num = 850;
+var num = 850; 
 
 var rightMove = function () {
-	num += 100;
+	num += 150;
 	document.getElementById('ship').style.marginLeft = num + "px";
 }
 var leftMove = function () {
-	num -= 100;
+	num -= 150;
 	document.getElementById('ship').style.marginLeft = num + "px";
 }
-
 var rockPosition = function(){
-	rockMargin = Math.floor(Math.random()*5);
-	console.log(rockMargin);
-	document.getElementById('rockThing').style.marginLeft = rockArray[rockMargin] + "px";
+  rockMargin = Math.floor(Math.random()*5);
+  console.log(rockMargin);
+  document.getElementById('rockThing').style.marginLeft = rockArray[rockMargin] + "px";
 }
-rockPosition()
 
 
+var topSpace = 0;
+
+var movement = function() {
+	setInterval(function() {
+		check();
+		moveit();
+		console.log(topSpace);
+	},50);
+}
+
+var moveit = function() {
+	topSpace = topSpace + 10;
+	document.getElementById('rockThing').style.marginTop = topSpace +"px";
+}
+
+var check= function() {
+	if (topSpace > 300){
+		if ( num == rockMargin){
+			alert("Game Over") 
+		} else{
+			topSpace = 0;
+		
+		}
+
+	}
 
 }
-function leftArrowPressed() {
-	var element = document.getElementById("image1");
-	element.style.left = parseInt(element.style,left) - 5 + 'px';
+
+movement();
